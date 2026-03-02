b65dc536761073f2ffa75be117ddd947f4200c9a
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-03-02 14:30:16 +0600

    implement intersection observer for active nav link highlighting

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -14,3 +14,37 @@ export function navToggler() {
 
   toggleNav();
 }
+
+export function intersectionObserver() {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const sections = document.querySelectorAll("section");
+
+  function removeActive() {
+    navLinks.forEach((link) => {
+      link.classList.remove("is-active");
+    });
+  }
+
+  function activateLink(id) {
+    const activeLink = document.querySelector(`.nav-link[href="#${id}"]`);
+    if (activeLink) {
+      activeLink.classList.add("is-active");
+    }
+  }
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          removeActive();
+          activateLink(entry.target.id);
+        }
+      });
+    },
+    {
+      rootMargin: "-45% 0px -45% 0px",
+    },
+  );
+
+  sections.forEach((section) => observer.observe(section));
+}
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,11 +1,12 @@
 import "../components/header.js";
-import { navToggler } from "../components/navbar.js";
+import { navToggler, intersectionObserver } from "../components/navbar.js";
 import {
   themeBtnToggler,
   applyTheme,
   activeThemeOption,
 } from "../components/theme.js";
 
+intersectionObserver();
 navToggler();
 themeBtnToggler();
 applyTheme();
