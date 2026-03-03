5132bd13cb3fa1a41fdf05d563df71fbb6069357
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-03-03 17:59:39 +0600

    implement navbar toggle functionality with overlay and active states

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -2,12 +2,18 @@ export function navToggler() {
   const togglers = document.querySelectorAll("[data-nav-toggler]");
   const navbar = document.querySelector("[data-navbar]");
   const overlay = document.querySelector("[data-overlay]");
+  const navbarOverlay = document.querySelector(".navbar-overlay");
+  const navbarList = document.querySelector(".navbar-list");
+  const closeMenuBtn = document.querySelector(".close-menu-btn");
 
   function toggleNav() {
     togglers.forEach((toggler) => {
       toggler.addEventListener("click", () => {
         navbar.classList.toggle("nav-active");
         overlay.classList.toggle("active");
+        navbarOverlay.classList.toggle("active");
+        navbarList.classList.toggle("navbar-list__active");
+        closeMenuBtn.classList.toggle("close-menu-btn__active");
       });
     });
   }
