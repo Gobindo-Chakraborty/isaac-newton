2067fcd27d93fced607b735a95702b41fe8acab6
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-03-18 22:20:36 +0600

    enhance responsiveness and layout across various components

--- js/pages/dive-deeper.js
+++ js/pages/dive-deeper.js
@@ -1,4 +1,12 @@
 import "../components/header.js";
 import { navToggler } from "../components/navbar.js";
+import {
+  themeBtnToggler,
+  applyTheme,
+  activeThemeOption,
+} from "../components/theme.js";
 
 navToggler();
+themeBtnToggler();
+applyTheme();
+activeThemeOption();
